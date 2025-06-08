e3af451392162d03143f222f669916fdcab2fadb
TAINGUYENNGOC\LENOVO <nguyenngoctai201202@gmail.com>
2025-06-08 18:29:58 +0700

    first commit

---
+++ src/test/java/com/project/java_spring_laptopshop/JavaSpringLaptopshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.java_spring_laptopshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringLaptopshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
